d39e5f91a98d24ceb95e4747b6dd480560eb2cbd
debian <debian@debian.debian>
2017-10-30 12:42:41 -0400

    fixes

--- analysis/get_seq_data.py
+++ analysis/get_seq_data.py
@@ -17,24 +17,30 @@ def get_seq_data(data_input_dh,data_input_fns):
     log_fh="%s.get_seq_data.log" % data_input_dh
     log_f = open(log_fh,'a')
     for data_input_fn in data_input_fns:
-        data_input_fh="%s/%s" % (data_input_dh,data_input_fn)
-        if not exists('%s.bz2' % data_input_fh):
-            logging.info("downloading: %s [%d/%d]" % (basename(data_input_fh),data_input_fns.index(data_input_fn)+1,len(data_input_fns)))
-            com="wget -q ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA082/SRA082074/%s.bz2 --directory-prefix=%s" % (data_input_fn,data_input_dh)
-            sub_call_err=subprocess.call(com,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
-            if sub_call_err!=0:
-                #ENA: eg. ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR129/001/SRR1292901/SRR1292901_1.fastq.gz
-                data_input_fn=data_input_fn.split('/')[1]
-                com="wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/%s/001/%s/%s.gz --directory-prefix=%s" % (data_input_fn[:6],data_input_fn[:10],data_input_fn,data_input_dh)
-                logging.info('bash command: $ %s' % com)
-                subprocess.call(com,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
-    subprocess.call("bzip2 -d %s/*.bz2" % data_input_dh ,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
+        data_input_fh="%s/%s" % (data_input_dh,basename(data_input_fn))
+        if not exists(data_input_fh):
+            if not exists('%s.bz2' % data_input_fh):
+                logging.info("downloading: %s [%d/%d]" % (basename(data_input_fh),data_input_fns.index(data_input_fn)+1,len(data_input_fns)))
+                com="wget -q ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/SRA082/SRA082074/%s.bz2 --directory-prefix=%s" % (data_input_fn,data_input_dh)
+                sub_call_err=subprocess.call(com,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
+                subprocess.call("bzip2 %s/%s.bz2" % (data_input_dh,data_input_fn.split('/')[1]) ,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
+                if sub_call_err!=0:
+                    if not exists('%s.gz' % data_input_fh):                    
+                        logging.warning('DDBJ not working, trying ENA')
+                        #ENA: eg. ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR129/001/SRR1292901/SRR1292901_1.fastq.gz
+                        data_input_fn=data_input_fn.split('/')[1]
+                        com="wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/%s/00%s/%s/%s.gz --directory-prefix=%s" % (data_input_fn[:6],data_input_fn[9],data_input_fn[:10],data_input_fn,data_input_dh)
+                        logging.info('bash command: $ %s' % com)
+                        subprocess.call(com,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
+                        sub_call_err=subprocess.call("gunzip %s/%s.gz" % (data_input_dh,data_input_fn) ,shell=True,stdout=log_f, stderr=subprocess.STDOUT)
+                        if sub_call_err!=0:
+                            logging.warning('ENA too not working')
     log_f.close()
     
 # prj_dh=raw_input("Get input data for dataset [project_directory eg. Melnikov_et_al_2014]:")
 prj_dh=sys.argv[1]
 if exists(prj_dh):
-    if prj_dh=='Melnikov_et_al_2014':
+    if 'Melnikov_et_al_2014' in prj_dh:
         data_input_dh="%s/data_input" % prj_dh
         data_input_fns=[    "SRX547509/SRR1292901_1.fastq","SRX547509/SRR1292901_2.fastq",
                             "SRX547496/SRR1292881_1.fastq","SRX547496/SRR1292881_2.fastq",
