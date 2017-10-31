271cbef07150b9ad53d8f85098960e19b90feff7
rraadd88 <rraadd_8@hotmail.com>
2017-10-31 07:05:30 -0400

    fixes

--- get_output.sh
+++ get_output.sh
@@ -2,7 +2,8 @@ prj_dhs=(Olson_et_al_2014 Firnberg_et_al_2014 Melnikov_et_al_2014)
 for prj_dh in ${prj_dhs[@]};
 do
 echo $prj_dh
-cp -r analysis/$prj_dh/data_comparison/ outputs/$prj_dh/
+cp -fr analysis/$prj_dh/data_comparison/ outputs/$prj_dh/
 mkdir outputs/$prj_dh/plots
-cp -r analysis/$prj_dh/plots/aas outputs/$prj_dh/plots
+echo "cp -r analysis/$prj_dh/plots/aas outputs/$prj_dh/plots"
+cp -fr analysis/$prj_dh/plots/aas outputs/$prj_dh/plots
 done 
